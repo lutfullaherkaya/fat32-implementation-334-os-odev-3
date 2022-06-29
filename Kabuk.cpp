71e8163e566e907fac43cec32e1d054c05f70720
lutfullah <lerkaya0@gmail.com>
2022-06-29 10:36:47 +0300

    dosyayı olmayan yere mv yapmayı engelledim.

--- Kabuk.cpp
+++ Kabuk.cpp
@@ -86,46 +86,48 @@ void Kabuk::touch(std::vector<string> &arglar) {
 }
 
 void Kabuk::mv(std::vector<string> &arglar) {
+    Dizin hedefDizin(suAnkiDizin);
+    if (!hedefDizin.git(arglar[2])) {
+        promptYaz();
+        return;
+    }
     vector<string> gidilecekDizinKelimeleri = ayir(arglar[1], '/');
     if (!gidilecekDizinKelimeleri.empty() && arglar.size() == 3) {
         auto listelemeDizini = Dizin(suAnkiDizin);
         string dosyaAdi = gidilecekDizinKelimeleri.back();
         gidilecekDizinKelimeleri.pop_back();
 
-        if (listelemeDizini.git(gidilecekDizinKelimeleri)) {
-            if (listelemeDizini.git(dosyaAdi)) {
-                pair<vector<FatFileLFN>, FatFile83> dizin = listelemeDizini.dizin;
-                listelemeDizini.ustDizineCik();
-
-                if (listelemeDizini.fatFileEntrySil(dosyaAdi)) {
-                    Dizin hedefDizin(suAnkiDizin);
-                    if (hedefDizin.git(arglar[2])) {
-                        vector<FatFileEntry> entriler;
-                        for (auto &lfn: dizin.first) {
-                            FatFileEntry ffn;
-                            ffn.lfn = lfn;
-                            entriler.push_back(ffn);
-                        }
-                        FatFileEntry ffn;
-                        ffn.msdos = dizin.second;
-                        entriler.push_back(ffn);
-                        hedefDizin.dizinEntrileriEkle(entriler);
-                        auto anaAdi = hedefDizin.dizinClusterID;
-                        hedefDizin.git(dosyaAdi);
-
-
-                        fseek(sisko32.imajFP, sisko32.clusterBaytAdresi(hedefDizin.dizinClusterID)+sizeof(FatFile83), SEEK_SET);
-                        FatFile83 noktaNokta;
-                        fread(&noktaNokta, sizeof(FatFile83), 1, sisko32.imajFP);
-
-                        noktaNokta.firstCluster = anaAdi & 0x0000FFFF;
-                        noktaNokta.eaIndex = anaAdi << 16;
-
-                        fseek(sisko32.imajFP, sisko32.clusterBaytAdresi(hedefDizin.dizinClusterID)+sizeof(FatFile83), SEEK_SET);
-                        fwrite(&noktaNokta, sizeof(FatFile83), 1, sisko32.imajFP);
-                    }
+        if (listelemeDizini.git(gidilecekDizinKelimeleri) && listelemeDizini.git(dosyaAdi)) {
+            pair<vector<FatFileLFN>, FatFile83> dizin = listelemeDizini.dizin;
+            listelemeDizini.ustDizineCik();
+            if (listelemeDizini.fatFileEntrySil(dosyaAdi)) {
+                vector<FatFileEntry> entriler;
+                for (auto &lfn: dizin.first) {
+                    FatFileEntry ffn;
+                    ffn.lfn = lfn;
+                    entriler.push_back(ffn);
                 }
+                FatFileEntry ffn;
+                ffn.msdos = dizin.second;
+                entriler.push_back(ffn);
+                hedefDizin.dizinEntrileriEkle(entriler);
+                auto anaAdi = hedefDizin.dizinClusterID;
+                hedefDizin.git(dosyaAdi);
+
+
+                fseek(sisko32.imajFP, sisko32.clusterBaytAdresi(hedefDizin.dizinClusterID) + sizeof(FatFile83),
+                      SEEK_SET);
+                FatFile83 noktaNokta;
+                fread(&noktaNokta, sizeof(FatFile83), 1, sisko32.imajFP);
+
+                noktaNokta.firstCluster = anaAdi & 0x0000FFFF;
+                noktaNokta.eaIndex = anaAdi << 16;
+
+                fseek(sisko32.imajFP, sisko32.clusterBaytAdresi(hedefDizin.dizinClusterID) + sizeof(FatFile83),
+                      SEEK_SET);
+                fwrite(&noktaNokta, sizeof(FatFile83), 1, sisko32.imajFP);
             }
+
         }
     }
     promptYaz();
